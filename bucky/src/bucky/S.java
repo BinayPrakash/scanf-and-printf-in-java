996150559b73528de94bdb6c804e8de76fe4f771
Bhaskar Kumar <kbhaskarl40@gmail.com>
2017-08-07 00:27:28 +0530

    This will help you to use scanf and printf in java

---
+++ bucky/src/bucky/S.java
@@ -0,0 +1,39 @@
+package bucky;
+import java.util.*;
+
+public class S {
+	public static String scanS()
+	{
+		Scanner p=new Scanner(System.in);
+				String a=p.next();
+		return a;
+	}
+	public static int scanI()
+	{
+		Scanner p=new Scanner(System.in);
+				int b=p.nextInt();
+		return b;
+	}
+	public static float scanF()
+	{
+		Scanner p=new Scanner(System.in);
+				float c=p.nextFloat();
+		return c;
+	}
+	public static void printf(String x)
+	{
+		System.out.println(x);
+		
+		}
+
+		public static void printf(int a)
+		{
+		System.out.println(a);
+		}
+
+		public static void printf(float a)
+		{
+		System.out.println(a);
+		}
+	
+}
---
+++ bucky/src/bucky/scanS.java
@@ -0,0 +1,15 @@
+package bucky;
+
+import bucky.S;
+public class scanS {
+
+	public static void main(String[] args) {
+		System.out.println("Enter a String below:");
+		String a;
+		a=S.scanS(); //using scan.S instead of Scanner
+		System.out.println("Entered String is "+a);
+		S.printf("Code by Bhaskar 'HAXONIC' ");
+		
+	}
+
+}
